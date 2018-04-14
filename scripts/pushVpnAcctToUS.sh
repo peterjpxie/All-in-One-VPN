845e261fd589d5e83f439fc2f356c43f89abe6fc
peter.jp.xie@gmail.com <peter.jp.xie@gmail.com>
2018-04-14 21:10:33 +1000

    delete ToUS scripts

--- scripts/pushVpnAcctToUS.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Push to us.petersvpn.com: chap-secrets and ipsec/passwd
-ssh-keygen -R us.petersvpn.com
-scp -o StrictHostKeyChecking=no -i ~/sshKeys/putty_private.pem /etc/ppp/chap-secrets root@us.petersvpn.com:/etc/ppp/chap-secrets 
-scp -i ~/sshKeys/putty_private.pem /etc/ipsec.d/passwd root@us.petersvpn.com:/etc/ipsec.d/passwd 
-
