90aa6d566193a97ab524b2259e5bad74445365fd
peter.jp.xie@gmail.com <peter.jp.xie@gmail.com>
2018-06-09 14:51:35 +1000

    update

---
+++ scripts/update_guest_pwd.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+root_path=~
+#delete old user
+${root_path}/All-in-One-VPN/scripts/manageuser.sh -d -u guest
+#add guest user
+new_pwd=hackme$(date +%s |cut -c 7-10)
+${root_path}/All-in-One-VPN/scripts/manageuser.sh -a -u guest -p $new_pwd
+#update config file
+grep "^guest " /etc/ppp/chap-secrets | awk '{print $3}' > ${root_path}/Website/guest_pwd.conf
+cat ${root_path}/Website/guest_pwd.conf
