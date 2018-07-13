38d1f57f9f92ef5371f75637548fc90f6c48e3fe
peter.jp.xie@gmail.com <peter.jp.xie@gmail.com>
2018-07-13 21:03:11 +1000

    update

---
+++ scripts/sync2server.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Psg. to sg.petersvpn.com: chap-secrets and ipsec/passwd
+ssh-keygen -R sg.petersvpn.com
+scp -o StrictHostKeyChecking=no -i ~/sshKeys/putty_private.pem /etc/ppp/chap-secrets root@sg.petersvpn.com:/etc/ppp/chap-secrets 
+scp -i ~/sshKeys/putty_private.pem /etc/ipsec.d/passwd root@sg.petersvpn.com:/etc/ipsec.d/passwd 
+
