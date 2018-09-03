922a729efbd24d6ea3cf7c557e97b5f5c035c8da
peter.jp.xie <peter.jp.xie@gmail.com>
2018-09-03 20:35:43 +1000

    update

--- scripts/updateSvrAfterClone.sh
+++ scripts/updateSvrAfterClone.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+eth_interface=$(ifconfig |grep "Link encap:Ethernet" |awk '{print $1}')
 old_server_internal_ip=$(grep "left=" /etc/ipsec.conf | awk 'BEGIN{FS="="} {print $2}')
-new_server_internal_ip=$(ifconfig eth0 | awk 'BEGIN{FS=":"} /inet addr/{print $2}'|awk '{print $1}')
+new_server_internal_ip=$(ifconfig $eth_interface | awk 'BEGIN{FS=":"} /inet addr/{print $2}'|awk '{print $1}')
 echo "old_server_internal_ip: $old_server_internal_ip"
 echo "new_server_internal_ip: $new_server_internal_ip"
 #old_server_internal_ip="172.26.6.244"
@@ -12,5 +13,6 @@ sed -i "s/$old_server_internal_ip/$new_server_internal_ip/" /etc/rc.local
 sh /etc/rc.local
 service ipsec start
 service xl2tpd start 
+# Apply new IPTables rules. Add reboot just to make sure it loads iptables changes.
 iptables-restore < /etc/iptables.rules
-#reboot
+reboot
