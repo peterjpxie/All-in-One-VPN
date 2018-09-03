9df8b8c95a6d13f390860ba5658eaa3c506c1d58
peter.jp.xie <peter.jp.xie@gmail.com>
2018-09-03 20:41:22 +1000

    update

--- scripts/a.sh
+++ scripts/a.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 echo "executing a.sh."
+# User management
+tinyproxy_port=$(egrep "^Port " /etc/tinyproxy.conf |awk '{print $2}')
+echo "===============================================================================
+Congrats! VPN servers are ready.
+PSK (IPSec / L2TP): petersvpn
+To create VPN users for PPTP, IPSec, L2TP, run ./manageuser.sh.
+To create VPN client profiles for OpenVPN, run ./openvpn/openvpn-install.sh.
+Web proxy port: $tinyproxy_port
+==============================================================================="
+
 
 
 
--- scripts/installVpnAllIn1.sh
+++ scripts/installVpnAllIn1.sh
@@ -96,10 +96,11 @@ esac
 fi
 
 # User management
+tinyproxy_port=$(egrep "^Port " /etc/tinyproxy.conf |awk '{print $2}')
 echo "===============================================================================
 Congrats! VPN servers are ready.
 PSK (IPSec / L2TP): petersvpn
 To create VPN users for PPTP, IPSec, L2TP, run ./manageuser.sh.
 To create VPN client profiles for OpenVPN, run ./openvpn/openvpn-install.sh.
-Web proxy port: 8888
+Web proxy port: $tinyproxy_port 
 ==============================================================================="
