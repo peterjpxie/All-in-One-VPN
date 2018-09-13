f063e6ad0bd255e7ce16e22014e46a27411a53ef
peter.jp.xie <peter.jp.xie@gmail.com>
2018-09-13 22:51:19 +1000

    update

--- scripts/updateSvrAfterClone.sh
+++ scripts/updateSvrAfterClone.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+# Need to check /etc/openvpn/server.conf next time
 eth_interface=$(ifconfig |grep "Link encap:Ethernet" |awk '{print $1}')
 old_server_internal_ip=$(grep "left=" /etc/ipsec.conf | awk 'BEGIN{FS="="} {print $2}')
 new_server_internal_ip=$(ifconfig $eth_interface | awk 'BEGIN{FS=":"} /inet addr/{print $2}'|awk '{print $1}')
@@ -6,9 +8,9 @@ echo "old_server_internal_ip: $old_server_internal_ip"
 echo "new_server_internal_ip: $new_server_internal_ip"
 #old_server_internal_ip="172.26.6.244"
 #new_server_internal_ip="172.26.1.210"
-sed -i "s/$old_server_internal_ip/$new_server_internal_ip/" /etc/ipsec.conf
-sed -i "s/$old_server_internal_ip/$new_server_internal_ip/" /etc/iptables.rules
-sed -i "s/$old_server_internal_ip/$new_server_internal_ip/" /etc/rc.local
+sed -i".old-$(date +%F-%T)" "s/$old_server_internal_ip/$new_server_internal_ip/" /etc/ipsec.conf
+sed -i".old-$(date +%F-%T)" "s/$old_server_internal_ip/$new_server_internal_ip/" /etc/iptables.rules
+sed -i".old-$(date +%F-%T)" "s/$old_server_internal_ip/$new_server_internal_ip/" /etc/rc.local
 
 sh /etc/rc.local
 service ipsec start
