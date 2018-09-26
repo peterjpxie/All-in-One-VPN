4ddde6399a7faf88de7a149ea9ebd7b0d0a0ab16
peter.jp.xie <peter.jp.xie@gmail.com>
2018-09-26 13:01:14 +1000

    update

--- scripts/l2tp/vpnsetup_v3.22.sh
+++ scripts/l2tp/vpnsetup_v3.22.sh
@@ -24,9 +24,9 @@
 # - All values MUST be placed inside 'single quotes'
 # - DO NOT use these special characters within values: \ " '
 
-YOUR_IPSEC_PSK=''
-YOUR_USERNAME=''
-YOUR_PASSWORD=''
+YOUR_IPSEC_PSK='petersvpn'
+YOUR_USERNAME='peter'
+YOUR_PASSWORD='peter'
 
 # Important notes:   https://git.io/vpnnotes
 # Setup VPN clients: https://git.io/vpnclients
