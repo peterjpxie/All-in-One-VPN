618d350540eb6acb8502a456a7e7f3e65d845b6c
peter.jp.xie <peter.jp.xie@gmail.com>
2018-12-23 16:41:07 +1100

    update

--- scripts/update_guest_pwd.sh
+++ scripts/update_guest_pwd.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+root_path=~
+# Default settings
+pwd_prefix=""
+pwd_len=4
+
+# Read settings from ini file
+ini_file=${root_path}/All-in-One-VPN/scripts/update_guest_pwd.ini
+if [ -f $ini_file ]; then
+    pwd_prefix=$(grep pwd_prefix $ini_file |awk -F "=" '{print $2}' |tr -d ' ')
+    pwd_len=$(grep pwd_len $ini_file |awk -F "=" '{print $2}' |tr -d ' ')
+fi
+pwd_cut_start_pos=$((10 - pwd_len + 1))
+new_pwd=$pwd_prefix$(date +%s |cut -c ${pwd_cut_start_pos}-10)
+# echo "prefix is $pwd_prefix, len is $pwd_len, start_pos is $pwd_cut_start_pos,new_pwd is $new_pwd."
 
 # copy to web server
 if [ "$1" = "" ]; then
@@ -8,12 +22,12 @@ else
 webServer=$1
 fi
 
-root_path=~
+
 #delete old user
 ${root_path}/All-in-One-VPN/scripts/manageuser.sh -d -u guest
 #add guest user
-pwd_prefix="closingdown"
-new_pwd=$pwd_prefix$(date +%s |cut -c 7-10)
+# pwd_prefix="closingdown"
+# new_pwd=$pwd_prefix$(date +%s |cut -c 7-10)
 ${root_path}/All-in-One-VPN/scripts/manageuser.sh -a -u guest -p $new_pwd
 #update and sync guest_pwd.conf
 grep "^guest " /etc/ppp/chap-secrets | awk '{print $3}' > ${root_path}/Website/guest_pwd.conf
